4d77528e38b0b71d978a85f8978e4fde15a93689
ana1ouse <maximjhilin@gmail.com>
2025-10-30 00:11:20 +0300

    2.1

---
+++ ConsoleApplication2.c
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+
+/**
+ * @brief Расчитывает площадь поверхности грани куба
+ * @param a - значение параметра a
+ * @return рассчитанное значение
+*/
+double SGrani(const double a);
+
+/**
+ * @brief Расчитывает площадь поверхности всех граней куба
+ * @param a - значение параметра a
+ * @return рассчитанное значение
+*/
+double SVsehGraney(const double a);
+
+/**
+ * @brief Расчитывает объём куба
+ * @param a - значение параметра a
+ * @return рассчитанное значение
+*/
+double V(const double a);
+
+
+/**
+ * @brief точка входа в программу
+ * @return возвращает 0,если программа выполнена корректно
+ */
+int main() {
+	double a;
+	scanf("%a", &a);
+
+	printf("A = %lf\n", SGrani(a) );
+	printf("B = %lf\n", SVsehGraney(a) );
+	printf("C = %lf\n", V(a) );
+	return 0;
+}
+
+double SGrani(const double a)
+{
+	return a * a;
+
+}
+
+double SVsehGraney(const double a)
+{
+	return a * a * 6;
+
+}
+
+double V(const double a)
+{
+	return a * a * a;
+
+}
