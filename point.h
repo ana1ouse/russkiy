92e3ac3d18d1819d886cccbb27432137b04c0a87
Жилин Максим ТКИ-241 <maximjhilin@gmail.com>
2026-09-10 17:27:35 +0300

    point.h

--- point.h
+++ point.h
@@ -23,7 +23,7 @@ public:
     /**
      * @brief Конструктор точки
      */
-    Point(double x = 0, double y = 0, double z = 0);
+    Point(const double x = 0, const double y = 0, const double z = 0);
 
     /**
      * @brief Получение координаты X
